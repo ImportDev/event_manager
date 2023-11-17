db1580a3a04659b6451e1a0dd157e30cc485c109
Import Dev <sethharish43@gmail.com>
2023-11-17 05:03:27 +0000

    Practiced upto iteration-3

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -29,10 +29,156 @@
 #   puts name
 # end
 
-lines = File.readlines('event_attendees.csv')
-lines.each_with_index do |line,index|
-  next if index == 0
-  columns = line.split(",")
-  name = columns[2]
-  puts name
+# lines = File.readlines('event_attendees.csv')
+# lines.each_with_index do |line,index|
+#   next if index == 0
+#   columns = line.split(",")
+#   name = columns[2]
+#   puts name
+# end
+
+#Switching over to use the CSV library
+# require "csv"
+# puts "event initialized"
+# contents = CSV.open("event_attendees.csv", headers: true)
+# contents.each do |row|
+#   name = row[2]
+#   puts name
+# end
+
+#Accessing columns by their names
+
+# require "csv"
+# contents = CSV.read(
+#   "event_attendees.csv",
+#   headers: true,
+#   header_converters: :symbol
+# )
+
+# contents.each do |row|
+#   name = row[:first_Name]
+#   zipcode = row[:zipcode]
+#   puts "#{name} #{zipcode}"
+# end
+
+# Iteration 2: Cleaning up our zip codes
+
+
+# require 'csv'
+# puts 'EventManager initialized.'
+# contents = CSV.open(
+#   'event_attendees.csv',
+#   headers: true,
+#   header_converters: :symbol
+# )
+
+# contents.each do |row|
+#   name = row[:first_Name]
+#   zipcode = row[:zipcode]
+
+#   if zipcode.nil?
+#     zipcode = '00000'
+#   elsif zipcode.length < 5
+#     zipcode = zipcode.rjust(5,"0")[0..4]
+#   elsif zipcode.length > 5
+#     zipcode = zipcode[0..4]
+#   end
+
+#   puts "#{name} #{zipcode}"
+# end
+
+#Iteration 3: Using Google’s Civic Information
+
+# require 'csv'
+# require 'google/apis/civicinfo_v2'
+
+
+# def clean_zipcode(zipcode)
+#   zipcode.to_s.rjust(5, '0')[0..4]
+# end
+
+# def legislators_by_zipcode(zip)
+#   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+#   civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+
+#   begin
+#     legislators = civic_info.representative_info_by_address(
+#       address: zip,
+#       levels: 'country',
+#       roles: ['legislatorUpperBody', 'legislatorLowerBody']
+#     )
+#     legislators = legislators.officials
+#     legislator_names = legislators.map(&:name)
+#     legislator_names.join(", ")
+#   rescue
+#     'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+#   end
+# end
+
+# puts 'EventManager initialized.'
+
+# contents = CSV.open(
+#   'event_attendees.csv',
+#   headers: true,
+#   header_converters: :symbol
+# )
+
+# contents.each do |row|
+#   name = row[:first_name]
+
+#   zipcode = clean_zipcode(row[:zipcode])
+
+#   legislators = legislators_by_zipcode(zipcode)
+
+#   puts "#{name} #{zipcode} #{legislators}"
+# end
+
+#Iteration 4: Form letters
+require 'csv'
+require 'google/apis/civicinfo_v2'
+
+
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5, '0')[0..4]
+end
+
+def legislators_by_zipcode(zip)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+
+  begin
+    legislators = civic_info.representative_info_by_address(
+      address: zip,
+      levels: 'country',
+      roles: ['legislatorUpperBody', 'legislatorLowerBody']
+    )
+    legislators = legislators.officials
+    legislator_names = legislators.map(&:name)
+    legislator_names.join(", ")
+  rescue
+    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  end
+end
+
+puts 'EventManager initialized.'
+
+contents = CSV.open(
+  'event_attendees.csv',
+  headers: true,
+  header_converters: :symbol
+)
+
+template_letter = File.read('form_letter.html')
+
+contents.each do |row|
+  name = row[:first_name]
+
+  zipcode = clean_zipcode(row[:zipcode])
+
+  legislators = legislators_by_zipcode(zipcode)
+
+  personal_letter = template_letter.gsub('FIRST_NAME', name)
+  personal_letter.gsub!('LEGISLATORS', legislators)
+
+  puts personal_letter
 end
